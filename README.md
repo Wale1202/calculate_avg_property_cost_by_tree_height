99e89ff56adf1929e04b5a6081258afb69ccb906
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2025-06-19 17:53:25 +0100

    initial skeleton

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # calculate_avg_property_cost_by_tree_height
+
+## Overview
+
+## How to Run
+1. 
---
+++ src/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double averageShort = 0.0; // replace with real calculation
+        double averageTall = 0.0;  // replace with real calculation
+
+        System.out.println("Average property price on short tree streets: €" + averageShort);
+        System.out.println("Average property price on tall tree streets: €" + averageTall);
+    }
+}
